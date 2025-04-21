09466abed16f77de292bab7cd2d5a17489278c28
DRM1961 <dirk.mellaerts@gmail.com>
2025-04-21 18:40:06 +0200

    add config file and mqtt states

---
+++ src_runtime/CConfig.py
@@ -0,0 +1,70 @@
+
+import yaml
+import os
+
+CONFIG_FILE = "config.yaml"
+
+class CConfig:
+    def __init__(self):
+        if os.path.exists(CONFIG_FILE):
+            with open(CONFIG_FILE, "r") as file:
+                self.config = yaml.safe_load(file)
+        else:
+            self.config = {
+                'mycounter': 1,
+                'headless': False,
+                'mqtt': {
+                    'broker': '172.21.235.33',
+                    'port': 1883,
+                    'pw': 'client',
+                    'user': 'rpi6'
+                },
+                'servo': {
+                    'iopin': 5,
+                    'min_duty': 2.2,
+                    'max_duty': 9.5,
+                },
+                'illum': {
+                    'brightness_autofocus': 0.1,
+                    'brightness_detect': 0.1,
+                    'brightness_measure': 0.1,
+                    'flashtime': 1.0,
+                },
+                'detection': {
+                    'min_mean_difference = 10'
+                }
+            }
+            with open(CONFIG_FILE, "w") as file:
+                yaml.safe_dump(self.config, file)
+
+    def GetMyCounter(self):
+        return(self.config['mycounter'])
+    def GetHeadlessMode(self):
+        return(self.config['headless'])
+    def GetMQTT(self):
+        return(self.config['mqtt'])
+    def GetServo(self):
+        return(self.config['servo'])
+    def GetIllum(self):
+        return(self.config['illum'])
+    def GetDetection(self):
+        return(self.config['detection'])
+
+
+
+
+if __name__ == "__main__":
+    myconfig = CConfig()
+    mycounter = myconfig.GetMyCounter()
+    print(mycounter)
+    headless = myconfig.GetHeadlessMode()
+    print(headless)
+    mqtt = myconfig.GetMQTT()
+    print(mqtt)
+    servo = myconfig.GetServo()
+    print(servo)
+    illum = myconfig.GetIllum()
+    print(illum)
+    brightness_autofocus = illum['brightness_autofocus']
+    detection = myconfig.GetDetection()
+    print(detection)
